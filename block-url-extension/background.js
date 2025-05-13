acdbf34e79e10bcd47355f0167f91ed7bcce7b12
ByTryHard <erroraccount@bk.ru>
2025-05-13 21:31:36 +0400

    Update background.js

--- block-url-extension/background.js
+++ block-url-extension/background.js
@@ -1,9 +1,12 @@
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
   chrome.storage.sync.get({ words: [] }, (data) => {
     const words = data.words;
-    const url = decodeURIComponent(details.url).toLocaleLowerCase(); 
+    const url = decodeURIComponent(details.url).toLocaleLowerCase();
 
-    if (words.some(word => url.includes(word.toLocaleLowerCase()))) {
+    // Remove special characters from URL. Remove - note/del line 7, line 9 cleanedUrl -> url
+    const cleanedUrl = url.replace(/[^a-zа-яё0-9]/gi, '');
+
+    if (words.some(word => cleanedUrl.includes(word.toLocaleLowerCase()))) {
       chrome.tabs.update(details.tabId, { url: "https://google.com" });
     }
   });
